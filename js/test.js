b9e90cfeff8454a4dadac00c5a21fb98786158c0
Masayoshi Wada <developer@andantesoftware.com>
2013-06-29 15:09:01 +0900

    added some tests

--- js/test.js
+++ js/test.js
@@ -1,3 +1,8 @@
 Smoothy.test(function(t){
   t.log('log test');
+  t.assert('assert nan string ok').nan('string');
+  t.assert('assert nan 6 fail').nan(6);
+  t.assert('assert not nan 6 ok').not().nan(6);
+  t.assert('assert not nan str fail').not().nan('string');
 });
+
